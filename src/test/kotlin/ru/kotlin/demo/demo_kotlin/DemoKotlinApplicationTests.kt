def3bff50e5a0f436edc8d37d3fd9c12be7eb934
Azat <Azateikin@icloud.com>
2023-09-14 13:57:10 +0500

    Первая сборка

---
+++ src/test/kotlin/ru/kotlin/demo/demo_kotlin/DemoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kotlin.demo.demo_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
